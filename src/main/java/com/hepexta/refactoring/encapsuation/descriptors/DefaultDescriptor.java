0b5f07a65bb72a4fcfd338fbf78447ac4364bdf7
gunya <hepexta@hotmail.com>
2018-12-08 20:44:43 +0300

    Encapsulate classes with factory. Make the class constructors non-public and let clients create instances of them using a Factory. Benefits and Liabilities
    +  Simplifies the creation of kinds of instances by making the set available through intention-revealing methods.
    
    +  Reduces the “conceptual weight” [Bloch] of a package by hiding classes that don’t need to be public.
    
    +  Helps enforce the mantra “program to an interface, not an implementation” [DP].
    
    –  Requires new/updated Creation Methods when new kinds of instances must be created.
    
    –  Limits customization when clients can only access a Factory’s binary code, not its source code.

--- src/main/java/com/hepexta/refactoring/encapsuation/descriptors/DefaultDescriptor.java
+++ src/main/java/com/hepexta/refactoring/encapsuation/descriptors/DefaultDescriptor.java
@@ -2,11 +2,7 @@ package com.hepexta.refactoring.encapsuation.descriptors;
 
 public class DefaultDescriptor extends AttributeDescriptor {
 
-    public DefaultDescriptor() {
-        super();
-    }
-
-    public DefaultDescriptor(String name, Class parent, Class type) {
+    protected DefaultDescriptor(String name, Class parent, Class type) {
         super(name, parent, type);
     }
 }
