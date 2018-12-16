fbd446dcdf566643c1f0224910bbc0828c66f9e3
gunya <hepexta@hotmail.com>
2018-12-16 18:29:36 +0300

    Extract Composite. Refactoring

--- src/main/java/com/hepexta/refactoring/generalization/extractcomposite/FormTag.java
+++ src/main/java/com/hepexta/refactoring/generalization/extractcomposite/FormTag.java
@@ -1,32 +1,9 @@
 package com.hepexta.refactoring.generalization.extractcomposite;
 
-import java.util.Enumeration;
-import java.util.Vector;
-
-public class FormTag extends Tag {
-
-    private Vector<Enumeration> allNodesVector;
+public class FormTag extends CompositeTag {
 
     public FormTag(String tagBegin, String tagEnd, String tagContents, String tagLine) {
         super(tagBegin, tagEnd, tagContents, tagLine);
     }
 
-    public String toPlainTextString() {
-        StringBuffer stringRepresentation = new StringBuffer();
-        Node node;
-
-        for (Enumeration e = getAllNodesVector().elements(); e.hasMoreElements();) {
-            node = (Node)e.nextElement();
-            stringRepresentation.append(node.toPlainTextString());
-        }
-        return stringRepresentation.toString();
-    }
-
-    public Vector<Enumeration> getAllNodesVector() {
-        return allNodesVector;
-    }
-
-    public void setAllNodesVector(Vector<Enumeration> allNodesVector) {
-        this.allNodesVector = allNodesVector;
-    }
 }
